da6a34a3ee8f37e64787d62e1cf0ca91ca617223
ArturoReyes23 <2022451030@teschi.edu.mx>
2024-10-22 16:38:47 -0600

    Plantilla Agregada

--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 # View for Login
 def login_views(request):
@@ -19,13 +21,43 @@ def login_views(request):
             login(request, user)
             return redirect('home')  # Redirecciona a la página principal
         else:
-            messages.error(request,'Invalid login credentials')
+            messages.error(request,'Invalid Login Credentials')
     return render (request,template_name)
 
 # View for Register
 def register_view(request):
     template_name = "auth-register.html"
     
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirmation = request.POST.get('password_confirmation')
+
+        """print(username)
+        print(email)
+        print(password)
+        print(password_confirmation)"""
+        
+        if password != password_confirmation:
+            messages.error(request, 'Las contraseñas no coinciden')
+            return render(request, template_name)
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'El usuario ya existe')
+            return render(request, template_name)
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'El correo electrónico ya existe')
+            return render(request, template_name)
+
+        user = User(
+            username=username,
+            email=email,
+            password=make_password(password)
+        )
+        user.save()
+        messages.success(request, 'Usuario creado con éxito')
     return render (request,template_name)
 
 # View for Forgot the Password
